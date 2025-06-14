1d5a02bf43b89c84a26dfcdc2e2c273dbbb3cc38
Raja <researchteambts6@gmail.com>
2025-06-14 11:11:42 +0530

    updation
    few footer improvements!!

--- Frontend/src/Components/Navigations/FooterGallery.jsx
+++ Frontend/src/Components/Navigations/FooterGallery.jsx
@@ -7,13 +7,13 @@ const FooterGallery = () => {
   return (
     <div 
       id="footer-social" 
-      className="bg-[#036e8d]/5 p-4 sm:p-6 md:p-8 lg:p-4 rounded-xl mt-6 sm:mt-0 w-fit shadow-sm transition-shadow duration-300 hover:shadow-md"
+      className="bg-[#036e8d]/5 p-6 sm:p-6 md:p-8 lg:p-4 rounded-xl mt-2 sm:mt-6 sm:mt-0 w-fit shadow-sm transition-shadow duration-300 hover:shadow-md"
     >
-      <h3 className="text-lg sm:text-xl md:text-2xl font-bubblegum text-[#036e8d] mb-4 sm:mb-6 md:mb-8 text-center tracking-wide">
+      <h3 className="text-lg sm:text-xl md:text-2xl font-bubblegum text-[#036e8d] mb-4 sm:mb-6 md:mb-8 text-left sm:text-center tracking-wide">
         Explore IIHH Gallery
       </h3>
       <div className="flex justify-center">
-        <div className="grid grid-cols-3 md:grid-cols-4 gap-2 sm:gap-3 md:gap-4 max-w-fit">
+        <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-3 md:gap-4 max-w-fit">
           {galleryImages.slice(0, window.innerWidth >= 768 ? 16 : 9).map((imgSrc, index) => (
             <div
               key={index}
